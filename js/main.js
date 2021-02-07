51e635a26a8f0d76e83c1ca1212250232b62dcf6
opalpeltzman <opalpeltzman@gmail.com>
2021-02-07 13:04:28 +0200

    update portfolio

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 
 
-    // document.getElementById("cv").onclick = function () {
-    //     window.open("C:\Users\opalp\Documents\opalpeltzman\OPAL PELTZMAN CV.pdf");
-    //     window.location.href = "C:\Users\opalp\Documents\opalpeltzman";
-    //     document.getElementById('C:\Users\opalp\Documents\opalpeltzman\OPAL PELTZMAN CV.pdf').click();
-    // };
-  // Start file download. 
-  document.getElementById("cv").addEventListener("click", function() { 
-    // Generate download of hello.txt file with some content 
-    var text = "OpalPeltzmanCV"; 
-    var filename = "OpalPeltzmanCV.pdf"; 
+  document.getElementById("cv").addEventListener("click", function() {
+    window.open("https://github.com/opalpeltzman");
 
-    download(filename, text); 
-}, false); 
+}, false);
